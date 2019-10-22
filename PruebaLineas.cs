9e615092d24ae1b6d62b2684591b2ae2ca059e24
David Alonso Loperana <dalonso@bside.com.mx>
2019-10-22 17:22:42 -0500

    nuevosarchivpss

---
+++ PruebaLineas.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+
+            Console.WriteLine("Utltima prueba de merge local");
+            Console.Read();
+
+            Console.WriteLine("prueba de merge local segundo mensaje");
+            Console.Read();
+
+            Console.WriteLine("Hola mundo mundial con comandos de git");
+            Console.Read();
+
+            Console.WriteLine("Utltima prueba de merge local");
+            Console.Read();
+
+            Console.WriteLine("prueba de merge local segundo mensaje");
+            Console.Read();
+
+            Console.WriteLine("master");
+            Console.Read();
+
+            Console.WriteLine("develop");
+            Console.Read();
+
+
+
+
+        }
+    }
+}
